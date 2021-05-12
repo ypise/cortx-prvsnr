94898ec22b3f3b08147d0872bcc321a7a7154b27
Divya Vijayakumar <divya.vijaykumar@seagate.com>
2021-05-12 20:04:56 +0530

    LR - basic skeletal structure
    Signed-off-by: Divya Vijayakumar <divya.vijaykumar@seagate.com>

---
+++ lr-cli/setup.py
@@ -0,0 +1,26 @@
+#
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+#
+
+from setuptools import setup
+
+setup(name='cortx-setup',
+      version='1.0',
+      description='Python cortx_setup API',
+      license='Seagate',
+      url='cortx_setup',
+      packages=['cortx-setup'],
+     )
